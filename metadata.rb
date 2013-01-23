db11fc8d8aba719ea580f086f6450936e3b82be6
shimizukawa <takayuki.shimizukawa@beproud.jp>
2013-01-23 11:18:36 +0900

    fix remove last 's' from 'essentials'

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ description      "Installs/Configures python-build"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-depends 'build-essentials'
+depends 'build-essential'
