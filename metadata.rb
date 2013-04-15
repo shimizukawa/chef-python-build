d8331b3c19fb42269117949a60f5981309059b8f
shimizukawa <takayuki.shimizukawa@beproud.jp>
2013-04-15 12:10:25 +0900

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "shimizukawa@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures python-build"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          "0.2.3"
 
 %w{ debian ubuntu centos suse fedora redhat }.each do |os|
   supports os
