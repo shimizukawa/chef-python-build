71e339110998152f09d76041122ad6c7ae8c5702
shimizukawa <shimizukawa@gmail.com>
2013-05-12 11:04:41 +0900

    remove notifies and actions to control processing (it was meening less)

--- definitions/python_build.rb
+++ definitions/python_build.rb
@@ -6,7 +6,6 @@ define :python_build, :action => :build, :install_prefix => '/usr/local', :owner
   archive_dir = Chef::Config[:file_cache_path]
   archive_file = "Python-#{version}.tar.bz2"
   if node["python_build"]["archive_url_base"]
-    #TODO: want to use string templating (it can use with ruby 1.9)
     archive_src = "#{node["python_build"]["archive_url_base"]}/#{archive_file}"
   else
     archive_src = "http://www.python.org/ftp/python/#{version}/#{archive_file}"
@@ -42,17 +41,14 @@ define :python_build, :action => :build, :install_prefix => '/usr/local', :owner
       user owner
       group group
       not_if {File.exists?("#{archive_dir}/#{archive_file}") || expected_python.call(install_target, version)}
-      notifies :run, "execute[extract-python-#{version}]"
     end
 
     execute "extract-python-#{version}" do
-      #action :nothing
       cwd archive_dir
       command "tar jxf #{archive_file}"
       user owner
       group group
       not_if {File.exists?("#{archive_dir}/Python-#{version}") || expected_python.call(install_target, version)}
-      notifies :run, "execute[configure-python-#{version}]"
     end
 
     cookbook_file "#{archive_dir}/Python-#{version}/py26-no-sslv2.patch" do
@@ -73,38 +69,31 @@ define :python_build, :action => :build, :install_prefix => '/usr/local', :owner
     end
 
     execute "configure-python-#{version}" do
-      #action :nothing
       cwd "#{archive_dir}/Python-#{version}"
       command "./configure --prefix=#{install_prefix}"
       user owner
       group group
       not_if {File.exists?("#{archive_dir}/Python-#{version}/Makefile") || expected_python.call(install_target, version)}
-      notifies :create_if_missing, "template[place-python-#{version}-setup.cfg]"
     end
 
     template "place-python-#{version}-setup.cfg" do
       action :create
-      #action :nothing
       path "#{archive_dir}/Python-#{version}/setup.cfg"
       source 'setup.cfg.erb'
       owner owner
       group group
       not_if {expected_python.call(install_target, version)}
-      notifies :run, "execute[make-python-#{version}]"
     end
 
     execute "make-python-#{version}" do
-      #action :nothing
       cwd "#{archive_dir}/Python-#{version}"
       command "make"
       user owner
       group group
       not_if {expected_python.call(install_target, version)}
-      notifies :run, "execute[make-install-python-#{version}]"
     end
 
     execute "make-install-python-#{version}" do
-      #action :nothing
       cwd "#{archive_dir}/Python-#{version}"
       command "make install"
       user owner
